228d0b50af8bd22d06633b0356c77e397bcabe61
IlyesAkb <zenzor37rus@yandex.ru>
2020-05-22 19:44:24 +0300

    create catalog page layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import './styles/index.scss'
+import noUiSlider from 'nouislider'
 
 const a = 20
+
+const slider = document.querySelector('.catalog__form-price-slider')
+
+noUiSlider.create(slider, {
+  start: [20, 80],
+  connect: [false, true, false],
+  margin: 10,
+  range: {
+    'min': [0],
+    'max': [100]
+  }
+})
 
 console.log(a)
